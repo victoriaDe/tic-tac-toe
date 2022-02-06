7dafe83d6e56316d66eabb41e2ed8f6eb1980158
vderugo <victoria.derugo@gmail.com>
2022-02-06 16:00:42 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SquareComponent} from './square/square.component';
+import {BoardComponent} from './board/board.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NbThemeModule, NbLayoutModule, NbButtonModule} from '@nebular/theme';
+import {NbEvaIconsModule} from '@nebular/eva-icons';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'cosmic'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
